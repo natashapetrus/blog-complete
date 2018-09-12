5c00e8c39b8e482d68be7952d43dbecb1a9b8c92
Natasha Petrus <natashatheo@hotmail.com>
2018-09-11 18:18:01 -0700

    using jekyll instead of buster

---
+++ Gemfile
@@ -0,0 +1,4 @@
+source 'https://rubygems.org'
+gem 'jekyll', group: :jekyll_plugins
+gem 'jekyll-sitemap', group: :jekyll_plugins
+gem 'jekyll-last-modified-at', group: :jekyll_plugins
--- buster.bat
+++
@@ -1,3 +0,0 @@
-buster.py generate --domain=http://localhost:2368
-fart -i -r "C:\Users\NatashaTheodora\Documents\GitHub\blog\static\*.html" http://localhost:2368 https://natashapetrus.github.io
-buster.py deploy
